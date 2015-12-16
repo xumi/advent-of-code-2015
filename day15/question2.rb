ee161c84f87edd960e569df748009231cd1aa598
Maxime Zielony <mzielony@domaintools.com>
2015-12-16 15:00:34 +0100

    add missing question 2 for day 15

---
+++ day15/question2.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+#
+# --- Day 15: Science for Hungry People ---
+#
+# --- Part Two ---
+# 
+# Your cookie recipe becomes wildly popular! Someone asks if you can make another recipe that has exactly 500 calories per cookie (so they can use it as a meal replacement). Keep the rest of your award-winning process the same (100 teaspoons, same ingredients, same scoring system).
+# 
+# For example, given the ingredients above, if you had instead selected 40 teaspoons of butterscotch and 60 teaspoons of cinnamon (which still adds to 100), the total calorie count would be 40*8 + 60*3 = 500. The total score would go down, though: only 57600000, the best you can do in such trying circumstances.
+# 
+# Given the ingredients in your kitchen and their properties, what is the total score of the highest-scoring cookie you can make with a calorie total of 500?
+#
+
+# Load the file containing our algorithm
+require File.join File.dirname(__FILE__), 'searcher'
+
+# Load our input from the input.txt file
+input  = File.read('./input.txt')
+
+# We define the calorie goal
+amount_of_calories = 500
+
+# Let's analyze!
+asnwer = search(input, amount_of_calories)
+
+# We display the result
+puts asnwer
--- day15/searcher.rb
+++ day15/searcher.rb
@@ -1,60 +1,85 @@
 #!/usr/bin/ruby
 
-def search(input)
-  ingredients_data = parse_ingredients(input)
-  ingredients   = ingredients_data.keys
+def search(input, calories_count=nil)
+  # We get the list of ingredients and their properties from the source
+  ingredients_data  = parse_ingredients(input)
+  # The ingredients list (names)
+  ingredients       = ingredients_data.keys
 
+  # The variable saving the best recipe yet
   best_recipe = {score: 0, recipe: nil}
 
+  # We find all the different dosages possible
   get_different_dosages(ingredients).each do |recipe|
     properties_values = {}
+
     ingredients.each_with_index do |ingredient, index|
+      # We get the nutritional values of this ingredient
       ingredient_values = ingredients_data[ingredient]
-      teaspoon_count = recipe[index]
+      # We get how many teaspoons we need for this recipe
+      teaspoon_count    = recipe[index]
+      # We get the nutritional value for each ingredient for each amount of teaspoons
       get_ingredient_values(ingredient_values, teaspoon_count).each do |property, sum|
+        # We initialize the value if needed
         properties_values[property] ||= 0
+        # We add the value to the count
         properties_values[property] += sum
       end
     end
+    # We calculate the score of this recipe
     total = 1
-    properties_values.values.each do |value|
+    properties_values.each do |property, value|
+      # We ignore calories for the score
+      next if property == :calories
+      # If we have a negative total then the whole score is 0
       total = 0 and break if value < 0
+      # We multiply all the values
       total *= value
     end
+    # We are picky about calories count (question 2) we check for that here
+    next if !calories_count.nil? && properties_values[:calories] != calories_count
+    # If this is the best recipe yet we save it
     if total > best_recipe[:score]
       best_recipe[:score]  = total
+      # Just for fun we want to save the recipe
       best_recipe[:recipe] = recipe.map.with_index{|amount, i| [ingredients[i],amount]}
     end
   end
-
-  # ------------------------------------------------------------
-  # DEBUG
-  puts '-'*30
-  puts "The Recipe is:"
-  puts best_recipe[:recipe].map{|r| r.join(' -> ')}
-  puts "With a score of: #{best_recipe[:score]}"
-  puts '-'*30
-  # ------------------------------------------------------------
-
+  # Let's display the recipe
+  puts best_recipe
+  # We return the best recipe score
   best_recipe[:score]
 end
 
 def get_ingredient_values(ingredient, teaspoon_count)
   values = {}
+  # For each ingredient we get the value for that amount of teaspoons
   ingredient.each do |property, value|
-    next if property == :calories
     values[property] = value * teaspoon_count
   end
   values
 end
 
 def get_different_dosages(ingredients)
-  valid_permutations = []
-  max_amount         = 100
-  (0..max_amount).to_a.permutation(ingredients.length).each do |permutation|
-    valid_permutations << permutation if permutation.inject(:+) == max_amount
+  # The exact amount of teaspoons we have to use for all recipes
+  max_amount = 100
+  # All the possible dosages that use exactly max_amount teaspoons
+  dosages = []
+  # We find the combinations
+  (0..max_amount).each do |i|
+    l = max_amount - i
+    next if l < 0
+    (0..max_amount).each do |j|
+      l = max_amount - i - j
+      next if l < 0
+      (0..max_amount).each do |k|
+        l = max_amount - i - j - k
+        next if l < 0
+        dosages << [i,j,k,l]
+      end
+    end
   end
-  valid_permutations
+  dosages
 end
 
 def parse_ingredients(list)
